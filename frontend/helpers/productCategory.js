3d2d488b31328afd43816cf083454e1076c4599b
Tarun <145962366+TtarunNDeveloper@users.noreply.github.com>
2024-07-23 16:54:21 +0530

    Delete frontend/helpers directory

--- frontend/helpers/productCategory.js
+++
@@ -1,7 +0,0 @@
-const productCategory = [
-    { id : 1, label : 'Milk', value : 'milk' },
-    { id : 2, label : 'Curd', value : 'curd' },
-    { id : 3, label : 'Sweets', value : 'sweets' }
-]
-
-export default productCategory;
