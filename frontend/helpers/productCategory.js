5a4b2b9cb0615ea6cea73837328fc946cab9fd39
Tarun <145962366+TtarunNDeveloper@users.noreply.github.com>
2024-07-23 12:30:49 +0530

    Create productCategory.js

---
+++ frontend/helpers/productCategory.js
@@ -0,0 +1,7 @@
+const productCategory = [
+    { id : 1, label : 'Milk', value : 'milk' },
+    { id : 2, label : 'Curd', value : 'curd' },
+    { id : 3, label : 'Sweets', value : 'sweets' }
+]
+
+export default productCategory;
